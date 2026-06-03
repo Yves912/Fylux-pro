a843edc0279f9532792b527f1f7a53867d99403a
Yves912 <matimbiayves@gmail.com>
2026-06-03 18:27:52 +0300

    Add files via upload
    Fylux-pro PWA v1.4

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'fylux-cache-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Installation du Service Worker et mise en cache des fichiers de base
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Intercepter les requêtes pour que le site fonctionne même hors-connexion
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
